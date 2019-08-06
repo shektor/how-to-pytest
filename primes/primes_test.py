9c44f2f2acf2c540dced2f18cb52e47ed2668c43
shektor <s.hektor@gmail.com>
2019-08-06 17:07:08 +0100

    Update test with parametrized function
    A parametrized function allows for the testing of multiple scenarios 
    using one function.
    
    A pytest decorator adds a parameter to the test function. The first 
    arguement contains a string of all the parameter names. The second 
    arguement is a list of tuples where each tuple is a test case.

--- primes/primes_test.py
+++ primes/primes_test.py
@@ -1,16 +1,11 @@
+import pytest
 from primes import is_prime
 
-def test_prime_low_number():
-    assert is_prime(1) == False
-
-def test_prime_prime_number():
-    assert is_prime(29)
-
-def test_prime_another_prime_number():
-    assert is_prime(2)
-
-def test_prime_a_third_prime_number():
-    assert is_prime(83)
-
-def test_prime_composite_number():
-    assert is_prime(15) == False
+@pytest.mark.parametrize('number,result', [
+    (1, False),
+    (15, False),
+    (29, True),
+    (83, True)
+])
+def test_is_prime(number, result):
+    assert is_prime(number) == result
