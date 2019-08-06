599eb667ff7d2dd0363f3f21e3b20a50c79ab76a
shektor <s.hektor@gmail.com>
2019-08-06 16:29:27 +0100

    Add prime number logic
    To determine whether a number T is prime or not the an algorithm can be 
    used. 
    
    Take the square root of the number T and round up to nearest integer to 
    get N. For every M number in the range 2 to N, divide the number T being 
    tested by M, and if the number is an integer as opposed to a float, then 
    T is not a prime number. But if none of the results are integers then 
    the number T is a prime number.

--- primes/primes.py
+++ primes/primes.py
@@ -1,3 +1,16 @@
+import math
+
 def is_prime(num):
     if num < 2:
         return False
+
+    num_sqrt = math.sqrt(num)
+    sqrt_round_up = math.ceil(num_sqrt)
+
+    for test_value in range(2, sqrt_round_up):
+        quotient = num / test_value
+
+        if quotient.is_integer():
+            return False
+
+    return True
--- primes/primes_test.py
+++ primes/primes_test.py
@@ -2,3 +2,15 @@ from primes import is_prime
 
 def test_prime_low_number():
     assert is_prime(1) == False
+
+def test_prime_prime_number():
+    assert is_prime(29)
+
+def test_prime_another_prime_number():
+    assert is_prime(2)
+
+def test_prime_a_third_prime_number():
+    assert is_prime(83)
+
+def test_prime_composite_number():
+    assert is_prime(15) == False
