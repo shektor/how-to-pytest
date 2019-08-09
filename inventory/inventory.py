ea689d43ca79dccc95ae2fb12a1ec03d0cccd6b8
shektor <s.hektor@gmail.com>
2019-08-09 15:17:02 +0100

    Add add_new_stock function to Inventory class
    The inventory system should hold a stocks dictionary object with the 
    add_new_stock function creating a new index key. The key should be 
    assigned a dictionary value including the price and quantity.
    
    The total_items attribute of the Inventory instance should also be 
    updated.
    
    Use a fixture to create an Inventory object for each test and prevent 
    repeated code.

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -3,3 +3,9 @@ class Inventory:
     def __init__(self, limit = 100):
         self.limit = limit
         self.total_items = 0
+        self.stocks = {}
+
+
+    def add_new_stock(self, item, price, quantity):
+        self.total_items += quantity
+        self.stocks[item] = { 'price': price, 'quantity': quantity }
--- inventory/inventory_test.py
+++ inventory/inventory_test.py
@@ -1,3 +1,4 @@
+import pytest
 from inventory import Inventory
 
 
@@ -13,3 +14,16 @@ def test_custom_inventory_limit():
 
     assert inventory.limit == 25
     assert inventory.total_items == 0
+
+
+@pytest.fixture
+def no_stock_inventory():
+    return Inventory(10)
+
+
+def test_add_new_stock_success(no_stock_inventory):
+    no_stock_inventory.add_new_stock('Test Jacket', 10.00, 5)
+
+    assert no_stock_inventory.total_items == 5
+    assert no_stock_inventory.stocks['Test Jacket']['price'] == 10.00
+    assert no_stock_inventory.stocks['Test Jacket']['quantity'] == 5
