db1ac734a5d0be855d324ef7cae2373c682e4691
shektor <s.hektor@gmail.com>
2019-08-09 17:07:54 +0100

    Merge add_new_stock function tests
    The add_new_stock tests can be merged, with the 'success' test added to 
    the bad input test by updating the parametrized function. The else 
    condition of the try block can then be used to assert a successful stock 
    addition.
    
    Fixtures can be used with parametrized functions, where it just needs to 
    be included as a test function parameter and not in the parametrize 
    decorator

--- inventory/inventory_test.py
+++ inventory/inventory_test.py
@@ -23,26 +23,20 @@ def no_stock_inventory():
     return Inventory(10)
 
 
-def test_add_new_stock_success(no_stock_inventory):
-    no_stock_inventory.add_new_stock('Test Jacket', 10.00, 5)
-
-    assert no_stock_inventory.total_items == 5
-    assert no_stock_inventory.stocks['Test Jacket']['price'] == 10.00
-    assert no_stock_inventory.stocks['Test Jacket']['quantity'] == 5
-
-
 @pytest.mark.parametrize('name, price, quantity, exception', [
     ('Test Jacket', 10.00, 0, InvalidQuantityException(
         'Cannot add a quantity of 0. All new stocks must have at least 1 item')),
     ('Test Jacket', 10.00, 25, NoSpaceException(
         'Cannot add these 25 items. Only 10 more items can be stored')),
+    ('Test Jacket', 10.00, 5, None),
 ])
-def test_add_new_stock_bad_input(name, price, quantity, exception):
-    inventory = Inventory(10)
+def test_add_new_stock(no_stock_inventory, name, price, quantity, exception):
     try:
-        inventory.add_new_stock(name, price, quantity)
+        no_stock_inventory.add_new_stock(name, price, quantity)
     except (InvalidQuantityException, NoSpaceException) as raisedIssue:
         assert isinstance(raisedIssue, type(exception))
         assert raisedIssue.args == exception.args
     else:
-        pytest.fail("Expected error but found none")
+        assert no_stock_inventory.total_items == 5
+        assert no_stock_inventory.stocks[name]['price'] == price
+        assert no_stock_inventory.stocks[name]['quantity'] == quantity
