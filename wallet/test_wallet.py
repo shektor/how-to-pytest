4d96c4c62718952089d50165ae801fb23f98e85e
shektor <s.hektor@gmail.com>
2019-10-22 22:39:07 +0100

    Use fixtures to create wallets

--- wallet/test_wallet.py
+++ wallet/test_wallet.py
@@ -3,29 +3,36 @@ import pytest
 from wallet import Wallet, InsufficientAmount
 
 
-def test_default_initial_amount():
-    wallet = Wallet()
-    assert wallet.balance == 0
+@pytest.fixture
+def empty_wallet():
+    '''Returns a Wallet instance with a zero balance'''
+    return Wallet()
 
 
-def test_setting_initial_amount():
-    wallet = Wallet(100)
+@pytest.fixture
+def wallet():
+    '''Returns a Wallet instance with a balance of 100'''
+    return Wallet(100)
+
+
+def test_default_initial_amount(empty_wallet):
+    assert empty_wallet.balance == 0
+
+
+def test_setting_initial_amount(wallet):
     assert wallet.balance == 100
 
 
-def test_add_to_balance():
-    wallet = Wallet()
-    wallet.add(50)
-    assert wallet.balance == 50
+def test_add_to_balance(empty_wallet):
+    empty_wallet.add(50)
+    assert empty_wallet.balance == 50
 
 
-def test_subtract_from_balance():
-    wallet = Wallet(10)
+def test_subtract_from_balance(wallet):
     wallet.subtract(5)
-    assert wallet.balance == 5
+    assert wallet.balance == 95
 
 
-def test_subtract_raises_exception_on_insufficient_balance():
-    wallet = Wallet()
+def test_subtract_raises_exception_on_insufficient_balance(empty_wallet):
     with pytest.raises(InsufficientAmount):
-        wallet.subtract(100)
+        empty_wallet.subtract(100)
