e0c357e38d1e5d76943623d6a72cd5b9585afb80
shektor <s.hektor@gmail.com>
2019-11-02 21:35:00 +0000

    Create _is_spare helper function
    Returns true or false based on whether a spare bowled in the frame.

--- bowling_game/bowling_game.py
+++ bowling_game/bowling_game.py
@@ -9,12 +9,15 @@ class Game:
 
     def final_score(self):
         score = 0
-        roll = 0
+        frame_index = 0
         for frame in range(10):
-            if self.rolls[roll] + self.rolls[roll + 1] == 10:
-                score += 10 + self.rolls[roll + 2]
+            if self._is_spare(frame_index):
+                score += 10 + self.rolls[frame_index + 2]
             else:
-                score += self.rolls[roll] + self.rolls[roll + 1]
-            roll += 2
+                score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+            frame_index += 2
 
         return score
+
+    def _is_spare(self, frame_index):
+        return self.rolls[frame_index] + self.rolls[frame_index + 1] == 10
