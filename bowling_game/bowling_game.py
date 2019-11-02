5225bb6e123b10bfda383cea0584fb03cd2566a3
shektor <s.hektor@gmail.com>
2019-11-02 22:14:28 +0000

    Refactor final_score function

--- bowling_game/bowling_game.py
+++ bowling_game/bowling_game.py
@@ -14,24 +14,31 @@ class Game:
         frame_index = 0
         for frame in range(self.FRAMES_IN_GAME):
             if self._is_strike(frame_index):
-                score += self.ALL_PINS
-                score += self.rolls[frame_index + 1]
-                score += self.rolls[frame_index + 2]
+                score += self.ALL_PINS + self._strike_bonus(frame_index)
                 frame_index += 1
                 continue
 
             if self._is_spare(frame_index):
-                score += self.ALL_PINS + self.rolls[frame_index + 2]
+                score += self.ALL_PINS + self._spare_bonus(frame_index)
             else:
-                score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+                score += self._sum_of_rolls_in_frame(frame_index)
             frame_index += 2
 
         return score
 
+    def _sum_of_rolls_in_frame(self, frame_index):
+        return self.rolls[frame_index] + self.rolls[frame_index + 1]
+
     def _is_spare(self, frame_index):
-        no_of_pins = self.rolls[frame_index] + self.rolls[frame_index + 1]
+        no_of_pins = self._sum_of_rolls_in_frame(frame_index)
 
         return no_of_pins == self.ALL_PINS
 
+    def _spare_bonus(self, frame_index):
+        return self.rolls[frame_index + 2]
+
     def _is_strike(self, frame_index):
         return self.rolls[frame_index] == self.ALL_PINS
+
+    def _strike_bonus(self, frame_index):
+        return self.rolls[frame_index + 1] + self.rolls[frame_index + 2]
