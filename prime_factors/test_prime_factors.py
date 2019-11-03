b8873f69d2d74d200106e81a9cd1f0f2239f0d8f
shektor <s.hektor@gmail.com>
2019-11-03 20:15:24 +0000

    Use fixture to create a PrimeFactors object

--- prime_factors/test_prime_factors.py
+++ prime_factors/test_prime_factors.py
@@ -1,13 +1,16 @@
+import pytest
+
 from prime_factors import PrimeFactors
 
 
-def test_one():
-    prime_factors = PrimeFactors()
+@pytest.fixture
+def prime_factors():
+    return PrimeFactors()
 
+
+def test_one(prime_factors):
     assert [] == prime_factors.generate(1)
 
 
-def test_two():
-    prime_factors = PrimeFactors()
-
+def test_two(prime_factors):
     assert [2] == prime_factors.generate(2)
