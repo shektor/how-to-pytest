bcebf14206225e75a773d091817a81853f8d5abd
shektor <s.hektor@gmail.com>
2019-11-03 20:43:51 +0000

    Add further tests with larger numbers

--- prime_factors/test_prime_factors.py
+++ prime_factors/test_prime_factors.py
@@ -15,7 +15,11 @@ def prime_factors():
     (4, [2, 2]),
     (6, [2, 3]),
     (8, [2, 2, 2]),
-    (9, [3, 3])
+    (9, [3, 3]),
+    (12, [2, 2, 3]),
+    (90, [2, 3, 3, 5]),
+    (147, [3, 7, 7]),
+    (330, [2, 3, 5, 11])
 ])
 def test_generate(prime_factors, number, prime_factors_list):
     assert prime_factors.generate(number) == prime_factors_list
